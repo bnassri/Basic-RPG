12d386a3de4d692b84989876696c108c399bb5a7
bnassri <82914301+bnassri@users.noreply.github.com>
2025-01-04 19:05:44 -0500

    Add new project for Engine

--- Engine/Models/Player.cs
+++ Engine/Models/Player.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Engine.Models
 {
-    internal class Player
+    class Player
     {
+        public string Name { get; set; }
+        public string CharacterClass { get; set; }
+        public int HitPoints { get; set; }
+        public int ExperiencePoints { get; set; }
+        public int Level { get; set; }
+        public int Gold { get; set; }
+        public Player()
+        {
+            Name = "N/A";
+            CharacterClass = "N/A";
+            HitPoints = 10;
+            ExperiencePoints = 0;
+            Level = 1;
+            Gold = 10;
+        }
     }
 }
