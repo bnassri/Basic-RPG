0621ccadf1bd62ba463cfe125bd9da1f2500782a
bnassri <82914301+bnassri@users.noreply.github.com>
2025-01-08 22:59:25 -0500

    update GameSession class to add location info

--- Engine/ViewModels/GameSession.cs
+++ Engine/ViewModels/GameSession.cs
@@ -10,7 +10,8 @@ namespace Engine.ViewModels
     public class GameSession
     {
         public Player CurrentPlayer { get; set; }
-        
+        public Location CurrentLocation { get; set; }
+
         public GameSession()
         {
             CurrentPlayer = new Player();
@@ -20,6 +21,12 @@ namespace Engine.ViewModels
             CurrentPlayer.HitPoints = 10;
             CurrentPlayer.ExperiencePoints = 0;
             CurrentPlayer.Level = 1;
+
+            CurrentLocation = new Location();
+            CurrentLocation.Name = "Home";
+            CurrentLocation.XCoordinate = 0;
+            CurrentLocation.YCoordinate = -1;
+            CurrentLocation.Description = "This is your home.";
 
         }
     }
