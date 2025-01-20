1b0d10966b6689c05734785450765715d48ad3da
bnassri <82914301+bnassri@users.noreply.github.com>
2025-01-19 21:57:29 -0500

    Add ItemFactory, GameItem, and Weapon classes
    Created ItemFactory.cs with ItemFactory class in Engine.Factory namespace to manage game item creation. Added GameItem.cs with GameItem class in Engine.Models namespace, representing general game items with properties and a Clone method. Added Weapon.cs with Weapon class in Engine.Models namespace, inheriting from GameItem and adding damage properties and a Clone method.

---
+++ Engine/Models/GameItem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class GameItem
+    {
+        public int ItemTypeID { get; }
+        public string Name { get; }
+        public string Price { get; }
+        public GameItem(int itemTypeID, string name, string price)
+        {
+            ItemTypeID = itemTypeID;
+            Name = name;
+            Price = price;
+        }
+
+        public GameItem Clone()
+        {
+            return new GameItem(ItemTypeID, Name, Price);
+        }
+    }
+}
---
+++ Engine/Models/Weapon.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine.Models
+{
+    public class Weapon : GameItem
+    {
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+
+        //instantate a weapon object with these params and pass values to GameItem constructor which will set the properties
+        public Weapon(int itemTypeID, string name, string price, int minDamage, int maxDamage)
+            : base(itemTypeID, name, price)
+        {
+            MinimumDamage = minDamage;
+            MaximumDamage = maxDamage;
+        }
+
+        public new Weapon Clone()
+        {
+            return new Weapon(ItemTypeID, Name, Price, MinimumDamage, MaximumDamage);
+        }
+    }
+}
