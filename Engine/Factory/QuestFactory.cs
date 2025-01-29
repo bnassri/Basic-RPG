c0260a84349bff5b239b91c088132f8d230e1b91
bnassri <82914301+bnassri@users.noreply.github.com>
2025-01-28 21:18:30 -0500

    Add QuestFactory class to manage quest data
    Added a new file `QuestFactory.cs` to the project and created the `Engine.Factory` namespace. Defined a static class `QuestFactory` with a private static readonly list `_quests` to store `Quest` objects. Implemented a static constructor to initialize quests with specific details. Added an internal static method `GetQuestByID` to retrieve quests by their ID.

---
+++ Engine/Factory/QuestFactory.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Engine.Models;
+
+namespace Engine.Factory
+{
+    internal static class QuestFactory
+    {
+        private static readonly List<Quest> _quests = new List<Quest>();
+
+        //use a static constructor to create the quests
+        static QuestFactory()
+        {
+            //Declare the items needed to complete the quest
+            List<ItemQuantity> itemsToComplete = new List<ItemQuantity>();
+            itemsToComplete.Add(new ItemQuantity(9001, 5));
+
+            //Declare the items that the player will receive as a reward
+            List<ItemQuantity> rewardItems = new List<ItemQuantity>();
+            rewardItems.Add(new ItemQuantity(1002, 1));
+
+            //Create the quest
+            _quests.Add(new Quest(1,
+                "Clear the herb garden",
+                "Defeat the snakes in the Herbalist's garden",
+                itemsToComplete,
+                25, 10,
+                rewardItems));
+        }
+        internal static Quest GetQuestByID(int id)
+        {
+            return _quests.FirstOrDefault(quest => quest.ID == id);
+        }
+    }
+}
